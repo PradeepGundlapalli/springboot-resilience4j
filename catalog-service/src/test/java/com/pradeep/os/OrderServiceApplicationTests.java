09e6cf19bf9fd2dc1656a1dc0211f675797c8abc
pc <pc@DESKTOP-A75FNB0>
2024-01-23 12:05:27 +0530

    o

--- catalog-service/src/test/java/com/pradeep/os/OrderServiceApplicationTests.java
+++ catalog-service/src/test/java/com/pradeep/os/OrderServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.javatechie.os;
+package com.pradeep.os;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
--- user-service/src/test/java/com/pradeep/us/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/pradeep/us/UserServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.javatechie.us;
+package com.pradeep.us;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
